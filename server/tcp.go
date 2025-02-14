d398fcd4c51a262c5f7af1548fb2e7cfe0745117
vlaner <vlaner@bk.ru>
2025-02-14 18:38:47 +0300

    add simple  tcp server

---
+++ server/tcp.go
@@ -0,0 +1,116 @@
+package server
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+type TCPServer struct {
+	Addr string
+	ln   net.Listener
+
+	connWg sync.WaitGroup
+	quit   chan struct{}
+}
+
+func NewServer(addr string) (*TCPServer, error) {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("server: listen on %s: %w", addr, err)
+	}
+
+	srv := &TCPServer{
+		Addr:   addr,
+		ln:     ln,
+		connWg: sync.WaitGroup{},
+		quit:   make(chan struct{}, 1),
+	}
+
+	return srv, nil
+}
+
+func (s *TCPServer) Start() {
+	go s.acceptLoop()
+}
+
+func (s *TCPServer) Stop(ctx context.Context) error {
+	listenErr := s.ln.Close()
+
+	s.quit <- struct{}{}
+
+	waitCh := make(chan struct{})
+	go func() {
+		// TODO: close all existing connections, otherwise this blocks
+		s.connWg.Wait()
+		waitCh <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("server: stop server: %w", errors.Join(listenErr, ctx.Err()))
+	case <-waitCh:
+		if listenErr != nil {
+			return fmt.Errorf("server: stop listener: %w", listenErr)
+		}
+
+		return nil
+	}
+}
+
+func (s *TCPServer) acceptLoop() {
+	for {
+		select {
+		case <-s.quit:
+			return
+		default:
+			conn, err := s.ln.Accept()
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) || errors.Is(err, io.EOF) {
+					return
+				}
+
+				log.Printf("server: accept connection: %v", err)
+				continue
+			}
+
+			tcpConn := conn.(*net.TCPConn)
+			tcpConn.SetKeepAlive(true)
+			tcpConn.SetKeepAlivePeriod(30 * time.Minute)
+
+			s.connWg.Add(1)
+			go s.handleConn(tcpConn)
+		}
+	}
+}
+
+func (s *TCPServer) handleConn(c net.Conn) {
+	defer c.Close()
+	defer s.connWg.Done()
+
+	r := bufio.NewReader(c)
+
+	for {
+		select {
+		case <-s.quit:
+			return
+		default:
+			bytesData, err := r.ReadBytes('\n')
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					return
+				}
+
+				log.Println("server: from read connection:", err)
+			}
+
+			log.Printf("server: received message %s\n", string(bytesData))
+		}
+	}
+}
---
+++ server/tcp_test.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestSimpleServer(t *testing.T) {
+	port := ":9090"
+	s, err := NewServer(port)
+	if err != nil {
+		t.Errorf("unexpected new server error: %v", err)
+	}
+
+	s.Start()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	clientConn, err := net.Dial("tcp", "127.0.0.1"+port)
+	if err != nil {
+		t.Errorf("unexpected dial error: %v", err)
+	}
+
+	_, err = clientConn.Write([]byte("TESTING\n"))
+	if err != nil {
+		t.Errorf("unexpected write to server error: %v", err)
+	}
+	clientConn.Close()
+
+	err = s.Stop(ctx)
+	if err != nil {
+		t.Errorf("unexpected stop server error: %v", err)
+	}
+}
